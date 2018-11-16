065f2394db744a5579a00b739e466b843cb3a499
Anu Khambete <anukhambete@gmail.com>
2018-11-16 02:13:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,4 +2,7 @@ function BoardMember(name, homestate, training) {
   this.name = name
   this.homeState = homestate
   this.training = training
+  this.veto = function(){
+    console.log("No, I must disagree");
+  }
 }
